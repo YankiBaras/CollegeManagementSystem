4e156eb5b33114054b1ccc09de8b1a3e13a0beaf
mendi <mendiby@gmail.com>
2023-06-25 14:49:38 +0300

    college change

--- college_test.py
+++ college_test.py
@@ -9,13 +9,13 @@ from class_room import ClassRoom
 
 def main():
     college = College('MeGo')
-    student1 = Student('Moshe', 'Tel Aviv', 30, 1)
-    teacher1 = Teacher('Hime', 'kfar saba', 60, 'math')
+    student1 = Student('Moshe', 1)
+    teacher1 = Teacher('Hime', 'math')
 
     college.add_student(student1)
     college.add_teacher(teacher1)
     for i in college.get_students():
-        print(i.student_id)
+        print(i.name)
 
 
 
