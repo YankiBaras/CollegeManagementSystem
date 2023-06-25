3ec7188078eb48f0897440c0f50e6b63ab7e2a31
mendi <mendiby@gmail.com>
2023-06-25 14:02:41 +0300

    test file

---
+++ college_test.py
@@ -0,0 +1,11 @@
+from student import Student
+from teacher import Teacher
+from grade import Grade
+from person import Person
+from college import College
+from course import Course
+from class_room import ClassRoom
+
+
+def main():
+    college = College('MeGo')
