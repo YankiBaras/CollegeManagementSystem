409ab7ba228d221dba1ce753d3d5da1a7a709f0c
Yanki Baras <y8463834@gmail.com>
2023-06-25 14:53:34 +0300

    fixing persons details and getters

--- person.py
+++ person.py
@@ -16,4 +16,4 @@ class Person:
         return self._personal_id
 
     def get_details(self):
-        return f"Name: {self._name}, Age: {self._personal_id}"
+        return f"Name: {self._name}, Personal_id: {self._personal_id}"
