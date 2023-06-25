09b0ebc35ed323d63ffcb3ea2869df5d6d3f8543
mendi <mendiby@gmail.com>
2023-06-25 14:16:59 +0300

    test file

--- college_test.py
+++ college_test.py
@@ -9,3 +9,11 @@ from class_room import ClassRoom
 
 def main():
     college = College('MeGo')
+    student1 = Student('Moshe', 'Tel Aviv', 30, 1)
+    teacher1 = Teacher('Hime', 'kfar saba', 60, 'math')
+
+    college.add_student(student1)
+    college.add_teacher(teacher1)
+
+
+main()
--- course.py
+++ course.py
@@ -1,9 +1,9 @@
 class Course:
 
-    def __init__(self, ID, course_name, course_teacher):
+    def __init__(self, ID, course_name, course_subject):
         self.ID = ID
         self.__course_name = course_name
-        self.__course_teacher = course_teacher
+        self.__course_subject = course_subject
         self.__course_students = []
 
     def add_student(self, student):
