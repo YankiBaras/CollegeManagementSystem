01768b2259c5214a9b50ccc9159d50d8df541fe0
mendi <mendiby@gmail.com>
2023-06-26 10:46:30 +0300

    class grade. program that add grades for each student

--- college_test.py
+++ college_test.py
@@ -19,6 +19,12 @@ def main():
     student1 = Student('Moshe', 1, 'A')
     student2 = Student('Michael', 4, 'B')
 
+    course1 = Course(1, 'java', ',Software')
+    course2 = Course(2, 'statistics', 'math')
+
+    course1.course_students = student1
+    course2.course_students = student2
+
     teacher1 = Teacher('Hime', 2, 'math')
     teacher2 = Teacher('Ronen', 3, 'English')
 
@@ -31,10 +37,16 @@ def main():
     college.teachers = teacher1
     college.teachers = teacher2
 
-    print(class_room_A.class_student.name)
-    print(class_room_A.class_student.personal_id)
-    print(class_room_A.class_student.classroom)
-    print(class_room_A.class_student.name)
+    student1_grades = Grade(1, 'Moshe')
+    student1_grades.update_grade(95, 'java')
+    student2_grades = Grade(4, 'Michael')
+    student2_grades.update_grade(80, 'statistics')
+
+    print(student1_grades.get_grades(1))
+    print(student2_grades.get_grades(4))
+    
+
+
 
 
 
--- grade.py
+++ grade.py
@@ -1,5 +1,18 @@
 class Grade:
-    def __init__(self, student, course, grade):
-        self._student = student
-        self._course = course
-        self._grade = grade
+    students_grades = []
+    def __init__(self, student_id, student_name):
+        self.__student_id = student_id
+        self.__student_name = student_name
+        self.__grade = {'student_id':self.__student_id, 'name': student_name}
+        Grade.students_grades.append(self.__grade)
+
+    def update_grade(self, grade, course):
+        self.__grade[course] = grade
+
+    def get_grades(self, student_id):
+        for dic in Grade.students_grades:
+            if dic['student_id'] == student_id:
+                return dic
+        return self.__grade
+
+
