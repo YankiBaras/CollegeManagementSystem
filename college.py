e8b48df8b166391c5bed525aa7a448bb23e81c31
Yanki Baras <y8463834@gmail.com>
2023-06-27 15:49:29 +0300

    fixing college class

--- college.py
+++ college.py
@@ -1,10 +1,12 @@
+from class_room import ClassRoom
+
+
 class College:
 
     def __init__(self, name):
         self.__name = name
-        self.__students = []
-        self.__teachers = []
-        self.classrooms = {}
+        self.classrooms = []
+        self.student_of_classroom = {}
 
     @property
     def name(self):
@@ -14,13 +16,17 @@ class College:
     def name(self, value):
         self.__name = value
 
+    def add_classroom(self, name):
+        self.classrooms.append(ClassRoom(name))
+        self.student_of_classroom[ClassRoom(name)] = ClassRoom(name).collection
 
     def remove_classroom(self, classroom):
         self.classrooms.remove(classroom)
 
     def get_class(self, personal_id):
-        swapped_dict = {student: cls for cls, students in College.classrooms.items() for student in students}
+        swapped_dict = {student: cls for cls, students in self.student_of_classroom.items() for student in students}
         for student in swapped_dict:
             if student.personal_id == personal_id:
                 return swapped_dict[student].name
+
 
